3dcf4bd8c5b5d4bb0b3863e5a2c8d0f1b97ff0e3
KarismaAgustiningtyas <109561631+KarismaAgustiningtyas@users.noreply.github.com>
2022-11-17 08:29:15 +0700

    Login

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+    $host       = "localhost";
+    $username   = "root"; 
+    $password   = "";
+    $db         = "user";
+
+    $koneksi = mysqli_connect($host, $username, $password, $db);
+
+    //$koneksi = mysqli_connect("localhost", "root", "", "user");
+
+    if (mysqli_connect_errno()) {
+        echo "Koneksi Gagal";
+    } //else { echo "Koneksi Berhasil"; }
+?>
