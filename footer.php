31504fa97d0a135018a9968002c7ee93fca80fe2
Vernhurn <yasinalfaruq02@gmail.com>
2022-11-30 13:08:54 +0700

    Update hf

--- footer.php
+++ footer.php
@@ -1,138 +1,85 @@
- <!-- Footer --> 
- <footer class="page-footer font-small mt-5" style="background-color: #4f94d4;">
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
 
-  <!-- Social buttons -->
-  <div style="background-color: #72aee6;" class="container-fluid">
-    <div class="container">
-      <!--Grid row-->
-      <div class="row py-4 d-flex align-items-center">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
 
-        <!--Grid column-->
-        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-          <h6 class="mb-0 white-text">Follow our social media!</h6>
-        </div>
-        <!--Grid column-->
-
-        <!--Grid column-->
-        <div class="col-md-6 col-lg-7 text-center text-md-right">
-          <!--Facebook-->
-          <a class="fb-ic ml-0" href="https://facebook.com">
-            <i class="fab fa-facebook white-text mr-4"> </i>
-          </a>
-          <!--Twitter-->
-          <a class="tw-ic" href="https://twitter.com">
-            <i class="fab fa-twitter white-text mr-4"> </i>
-          </a>
-          <!--Google +-->
-          <a class="gplus-ic" href="https://gmail.com">
-            <i class="fab fa-google-plus white-text mr-4"> </i>
-          </a>
-          <!--Linkedin-->
-          <a class="li-ic" href="https://www.linkedin.com/">
-            <i class="fab fa-linkedin white-text mr-4"> </i>
-          </a>
-          <!--Instagram-->
-          <a class="ins-ic" href="https://instagram.com">
-            <i class="fab fa-instagram white-text mr-lg-4"> </i>
-          </a>
-        </div>
-        <!--Grid column-->
-
-      </div>
-      <!--Grid row-->
-    </div>
-  </div>
-  <!-- Social buttons -->
-
-  <!--Footer Links-->
-  <div class="container mt-4 mb-4 text-center text-md-left">
-    <div class="row mt-3 wrapper">
-
-      <!--First column-->
-      <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
-        <img style="max-width: 200px;" src="img/Logo putih.PNG">
-        <p>Dessert is a dish that is generally eaten after the main menu. Desserts generally have a sweet taste and are served cold or hot. The function of dessert in a dish is as a refresher after eating the main course, which sometimes has a fishy aroma or taste.</p>
-      </div>
-      <!--/.First column-->
-
-      <!--Third column-->
-      
-      <div class="col-xl-2 mb-4 mx-auto">
-        <h6 style="color: white" class="text-uppercase font-weight-bold">
-          <strong>Useful links</strong>
-        </h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <a href="#!">Your Account</a>
-        </p>
-        <p>
-          <a href="#!">Become an Member</a>
-        </p>
-        <p>
-          <a href="#!">Shipping Rates</a>
-        </p>
-        <p>
-          <a href="#!">About</a>
-        </p>
-      </div>
-
-      <!--/.Third column-->
-
-      <!--Fourth column-->
-      <div class="col-xl-3">
-        <h6 style="color: white" class="text-uppercase font-weight-bold">
-          <strong>Contact</strong>
-        </h6>
-        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <i class="fas fa-home mr-2"></i>Jl. Mastrip 68121, Jember</p>
-          <p>
-            <i class="fa fa-phone mr-2"></i>+ 62 858 905 039 37 (Karisma)</p>
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <a href="index.html"><img src="assets/img/logo/logo.png" alt="" class="img-fluid" style="width: 150px;"></a>
+            <br><br>
             <p>
-              <i class="fa fa-phone mr-2"></i>+ 62 895 055 621 35 (Kresna)</p>
-              <p>
-                <i class="fa fa-phone mr-2"></i>+ 62 852 361 244 99 (Reihan)</p>
-                <p>
-                  <i class="fa fa-phone mr-2"></i>+ 62 831 222 072 32 (Yasin)</p>
-                </p>
-              </div>
-              <!--/.Fourth column-->
-
-            </div>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
           </div>
-          <!--/.Footer Links-->
 
-          <!-- Copyright -->
-          <div class="footer-copyright text-center py-3">&copy; 2022 Copyright - All rights reserved.
-            <a href="">Made By 2ⁿᵈ Group From MIF C</a>
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Categories</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
           </div>
-          <!-- Copyright -->
 
-        </footer>
-        <!-- /.Footer -->
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
 
-        <!-- SCRIPTS -->
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <!-- MDB core JavaScript -->
-        <script type="text/javascript" src="js/mdb.min.js"></script>
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>About Us</h4>
+            <p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.</p>
+          </div>
 
-        <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets1/js/jquery.js"></script>
-    <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
-    <script src="assets1/bootstrap/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug 
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>-->
-  
-  <script src="assets1/bootstrap/js/moment.js"></script>
-    <script src="assets1/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
-    <script type="text/javascript">
-      $(function () {
-        $('#datetimepicker').datetimepicker({
-          format: 'YYYY-MM-DD HH:mm',
-                });
-      });
-    </script>
-      </html>
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; 2022 Copyright <strong><span>WM Hana Asri</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          By <a href="#">Tim Catering</a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -1,63 +1,89 @@
+<?php
+  require ('koneksi.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Family Dessert</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>WM Hana Asri</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/carousel.css">
+  <link rel="stylesheet"href="assets/css/order.css" >
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
-  <!-- Material Design Bootstrap -->
-  <link href="css/mdb.min.css" rel="stylesheet">
-  <!-- Customized Bootstrap Stylesheet -->
-  <link href="css/style.css" rel="stylesheet">
-  <link href="img/Logo rumah hitam.PNG" rel="shortcut icon">
-  <!-- Bootstrap core CSS -->
-    <link href="assets1/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="assets1/bootstrap/css/datetimepicker.css" rel="stylesheet">
+
 </head>
+
 <body>
-  <!-- Main Navigation -->
-  <header>
 
-    <nav style="background: #4f94d4;" class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
 
-      <div class="container">
-
-        <a class="navbar-brand" href="#"><img style="max-width: 100px;" src="img/Logo putih.PNG"></a>
-        <!-- Collapse -->
-        <button class="navbar-toggler third-button collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" expanded="true" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-
-        <div class="collapse navbar-collapse " id="navbarSupportedContent-333">
-
-          <ul class="navbar-nav ml-auto nav-flex-icons">
-
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light" href="shopping_cart.php">
-                <i class="fa fa-shopping-cart mr-1"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light" href="index.php">
-                <i class="fa fa-home mr-1"></i>
-              </a>
-            </li>
-
-
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light" href="login.php">
-                <i class="fas fa-user mr-1"></i>
-              </a>
-            </li>
-
-
-
-          </ul>
-
-        </div>
-
+      <div class="logo me-auto">
+        <a href="index.html"><img src="assets/img/logo/logo.png" alt="" class="img-fluid"></a>
       </div>
-    </nav>
 
-  </header>
-  <!-- /.Main Navigation -->
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+
+          <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+          <li class="dropdown"><a href="#"><span>Kategori</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#"><span>Harian</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Senin</a></li>
+                  <li><a href="#">Selasa</a></li>
+                  <li><a href="#">Rabu</a></li>
+                  <li><a href="#">Kamis</a></li>
+                  <li><a href="#">Jumat</a></li>
+                  <li><a href="#">Sabtu</a></li>
+                  <li><a href="#">Minggu</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Mingguan</a></li>
+              <li><a href="#">Prasmanan</a></li>
+              <li><a href="#">Kotakan</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="input-group rounded m-3" style="width: 200px;">
+        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon"/>
+      </div>
+
+      <a href="#" class="js-search-open"><span class="bi-search"></span></a>
+
+      <button href="#" class="btn m-3">Log In</button>
+
+    </div>
+  </header><!-- End Header -->
+
