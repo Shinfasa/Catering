8ce3e3d84f8688b75c21dacdf190b988e31669e6
KarismaAgustiningtyas <109561631+KarismaAgustiningtyas@users.noreply.github.com>
2022-12-15 11:24:21 +0700

    ctg

---
+++ categories.php
@@ -0,0 +1,105 @@
+<?php
+    include "header.php";
+?>
+
+<br>
+<br>
+<br>
+
+<body>
+    
+<!-- popular section starts  -->
+<section class="popular" id="popular">
+
+    <div class="section-title">
+          <h2>Kategori Harian</h2>
+        </div>
+      <div class="box-container">
+  
+          <div class="box">
+              <span class="price">Rp 10.000</span>
+              <a href="product_detail.php"><img src="assets/img/p-1.jpg" alt=""></a>
+              <h3>tasty burger</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+              <a href="#" class="btn">order now</a>
+            </div>
+          <div class="box">
+            <span class="price">Rp 15.000</span>
+            <img src="assets/img/p-2.jpg" alt="">
+                <h3>tasty cakes</h3>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+              </div>
+              <a href="#" class="btn">order now</a>
+          </div>
+          <div class="box">
+              <span class="price">Rp 20.000</span>
+              <img src="assets/img/p-3.jpg" alt="">
+              <h3>tasty sweets</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+            </div>
+            <a href="#" class="btn">order now</a>
+        </div>
+          <div class="box">
+              <span class="price">Rp 25.000</span>
+              <img src="assets/img/p-4.jpg" alt="">
+              <h3>tasty cupcakes</h3>
+              <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+            </div>
+            <a href="#" class="btn">order now</a>
+        </div>
+        <div class="box">
+              <span class="price">Rp 30.000</span>
+              <img src="assets/img/p-5.jpg" alt="">
+              <h3>cold drinks</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+            </div>
+            <a href="#" class="btn">order now</a>
+        </div>
+        <div class="box">
+              <span class="price">Rp 35.000</span>
+            <img src="assets/img/p-6.jpg" alt="">
+            <h3>cold ice-cream</h3>
+            <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+              </div>
+              <a href="#" class="btn">order now</a>
+          </div>
+  
+    </div>
+</section> <!--- popular section end -->
+
+</body>
+
+<?php
+    include "footer.php";
+?>
