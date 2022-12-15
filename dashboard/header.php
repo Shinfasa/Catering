a24b1a0224e5edc7f1c6f7685390ce923e18169f
Vernhurn <yasinalfaruq02@gmail.com>
2022-12-15 13:15:15 +0700

    Update Dashboard Index

--- dashboard/header.php
+++ dashboard/header.php
@@ -137,7 +137,7 @@
             <li class="nav-item d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
+                <span class="d-sm-inline d-none">Log Out</span>
               </a>
             </li>
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
