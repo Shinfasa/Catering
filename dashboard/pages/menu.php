49682415fdfaa461b5ed03eb1a9529e677add487
Vernhurn <yasinalfaruq02@gmail.com>
2022-12-27 20:10:03 +0700

    Update menu.php

--- dashboard/pages/menu.php
+++ dashboard/pages/menu.php
@@ -6,7 +6,12 @@ include('header.php')
 
           <div class="card" style="height:100vh; max-height: 540px;">
             <div class="card-header pb-0">
-              <h6>User</h6>
+              <div class="d-flex align-items-center mb-3">
+                  <h6 class="mb-0">Menu</h6>
+                  <div class="text-end" style="flex: 0 0 auto; width: 96%;">
+                  <a href="#" class="btn btn-outline-primary btn-xs mb-0">+</a>
+                  </div>
+                </div>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
@@ -24,7 +29,7 @@ include('header.php')
                   </thead>
                   <tbody>
                     <?php 
-                      $batas = 10;
+                      $batas = 9;
                       $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
                       $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
  
@@ -59,47 +64,12 @@ include('header.php')
                     <?php
                       }
                     ?>
-                    <tr>
-                      <td class="text-center">2</td>
-                      <td>
-                        <div class="d-flex px-2 py-1">
-                          <div>
-                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
-                          </div>
-                          <div class="d-flex flex-column justify-content-center">
-                            <h6 class="mb-0 text-sm">John Michael</h6>
-                            <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <p class="text-xs font-weight-bold mb-0">Manager</p>
-                        <p class="text-xs text-secondary mb-0">Organization</p>
-                      </td>
-                      <td class="align-middle text-center text-sm">
-                        <span class="badge badge-sm bg-gradient-success">Online</span>
-                      </td>
-                      <td class="align-middle text-center">
-                        <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
-                      </td>
-                      <td></td>
-                      <td class="align-middle text-center">
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                          Edit
-                        </a>
-                        &nbsp;
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                          Delete
-                        </a>
-                      </td>
-                    </tr>
-
                   </tbody>
                 </table>
                 <nav>
                   <ul class="pagination justify-content-center">
                     <li class="page-item">
-                      <a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$Previous'"; } ?>> < </a>
+                      <a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$previous'"; } ?>> < </a>
                     </li>
                     <?php 
                     for($x=1;$x<=$total_halaman;$x++){
