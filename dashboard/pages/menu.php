b6781407f8c05b386dcc4a18c0ee9dbcbeae2b05
Vernhurn <yasinalfaruq02@gmail.com>
2022-12-27 09:43:32 +0700

    Update Menu dashboard

---
+++ dashboard/pages/menu.php
@@ -0,0 +1,143 @@
+<?php
+include('header.php')
+?>
+    <div class="container-fluid py-3">
+      <div class="row">
+
+          <div class="card" style="height:100vh; max-height: 540px;">
+            <div class="card-header pb-0">
+              <h6>User</h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Menu</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Harga</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Detail</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Gambar</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kategori</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php 
+                      $batas = 10;
+                      $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+                      $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
+ 
+                      $previous = $halaman - 1;
+                      $next = $halaman + 1;
+        
+                      $data = mysqli_query($koneksi,"SELECT * FROM menu JOIN kategori ON menu.id_kategori = kategori.id_kategori;");
+                      $jumlah_data = mysqli_num_rows($data);
+                      $total_halaman = ceil($jumlah_data / $batas);
+ 
+                      $data_pegawai = mysqli_query($koneksi,"SELECT * FROM menu JOIN kategori ON menu.id_kategori = kategori.id_kategori LIMIT $halaman_awal, $batas");
+                      $nomor = $halaman_awal+1;
+                      while($d = mysqli_fetch_array($data_pegawai)){
+                    ?>
+                      <tr>
+                        <td class="text-center"><?php echo $nomor++; ?></td>
+                        <td class="text-center"><h6 class="mb-0 text-sm"><?php echo $d['nama_menu']; ?></h6></td>
+                        <td class="text-center"><?php echo $d['harga']; ?></td>
+                        <td class="text-center"><?php echo $d['detail']; ?></td>
+                        <td class="text-center"><?php echo $d['gambar']; ?></td>
+                        <td class="text-center"><?php echo $d['nama_kategori']; ?></td>
+                        <td class="align-middle text-center">
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                          </a>
+                          &nbsp;
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                            Delete
+                          </a>
+                        </td>
+                      </tr>
+                    <?php
+                      }
+                    ?>
+                    <tr>
+                      <td class="text-center">2</td>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          <div>
+                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
+                          </div>
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">John Michael</h6>
+                            <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p class="text-xs font-weight-bold mb-0">Manager</p>
+                        <p class="text-xs text-secondary mb-0">Organization</p>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-success">Online</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
+                      </td>
+                      <td></td>
+                      <td class="align-middle text-center">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                        </a>
+                        &nbsp;
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Delete
+                        </a>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+                <nav>
+                  <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                      <a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$Previous'"; } ?>> < </a>
+                    </li>
+                    <?php 
+                    for($x=1;$x<=$total_halaman;$x++){
+                      ?> 
+                      <li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+                      <?php
+                    }
+                    ?>        
+                    <li class="page-item">
+                      <a  class="page-link" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?>>> </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+</body>
+
+</html>
