5c6c5ea4add0c1e2442a5d91eb661f376c194389
Vernhurn <yasinalfaruq02@gmail.com>
2022-12-27 09:38:38 +0700

    Update order.php

--- dashboard/pages/order.php
+++ dashboard/pages/order.php
@@ -20,11 +20,57 @@ include('header.php');
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Menu</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Catatan</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Harga Satuan</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jumlah</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Harga</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pembayaran</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tgl. Bayar</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status Pesanan</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
                     </tr>
                   </thead>
                   <tbody>
+                    <?php 
+                      $batas = 10;
+                      $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+                      $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
+ 
+                      $previous = $halaman - 1;
+                      $next = $halaman + 1;
+        
+                      $data = mysqli_query($koneksi,"SELECT * FROM order JOIN orderdetail ON orderdetail.id_order = order.id_order JOIN user ON order.id_user = user.id_user JOIN menu ON order.id_menu = menu.id_menu;");
+                      $jumlah_data = mysqli_num_rows($data);
+                      $total_halaman = ceil($jumlah_data / $batas);
+ 
+                      $data_order = mysqli_query($koneksi,"SELECT * FROM order JOIN orderdetail ON order.id_order = orderdetail.id_order JOIN user ON order.id_user = user.id_user JOIN menu ON order.id_menu = menu.id_menu LIMIT $halaman_awal, $batas");
+                      $nomor = $halaman_awal+1;
+                      while($d = mysqli_fetch_array($data_order)){
+                    ?>
+                      <tr>
+                        <td class="text-center"><?php echo $nomor++; ?></td>
+                        <td class="text-center"><h6 class="mb-0 text-sm"><?php echo $d['nama_user']; ?></h6></td>
+                        <td class="text-center"><?php echo $d['tgl_pesan']; ?></td>
+                        <td class="text-center"><?php echo $d['tgl_pakai']; ?></td>
+                        <td class="text-center"><?php echo $d['nama_menu']; ?></td>
+                        <td class="text-center"><?php echo $d['catatan_order']; ?></td>
+                        <td class="text-center"><?php echo $d['harga_satuan']; ?></td>
+                        <td class="text-center"><?php echo $d['jumlah']; ?></td>
+                        <td class="text-center"><?php echo $d['total_harga']; ?></td>
+                        <td class="text-center"><?php echo $d['metode_pembayaran']; ?></td>
+                        <td class="text-center"><?php echo $d['tgl_bayar']; ?></td>
+                        <td class="text-center"><?php echo $d['status_pesanan']; ?></td>
+                        <td class="align-middle text-center">
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                          Edit
+                          </a>
+                          &nbsp;
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                            Delete
+                          </a>
+                        </td>
+                      </tr>
+                    <?php
+                      }
+                    ?>
                     <tr>
                       <td class="text-center">1</td>
                       <td>
