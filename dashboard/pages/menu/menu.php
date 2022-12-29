9b6bdc5808ec24fe0b26782680f2a2afe6840394
Lauraaa20 <laurafigustina02@gmail.com>
2022-12-29 21:02:49 +0700

    update

--- dashboard/pages/menu/menu.php
+++ dashboard/pages/menu/menu.php
@@ -1,5 +1,5 @@
 <?php
-include('header.php')
+include('../header.php')
 ?>
     <div class="container-fluid py-3">
       <div class="row">
@@ -51,7 +51,7 @@ include('header.php')
                         <td class="text-center" style="width:10px; max-width:10px;"><?php echo $d['detail']; ?></td>
                         <td class="text-center">
                           <div>
-                            <img src="../../assets/img/menu/<?php echo $d['gambar']; ?>" class="avatar avatar-xxl me-3">
+                            <img src="../../../assets/img/menu/<?php echo $d['gambar']; ?>" class="avatar avatar-xxl me-3">
                           </div></td>
                         <td class="text-center"><?php echo $d['nama_kategori']; ?></td>
                         <td class="align-middle text-center">
@@ -93,25 +93,6 @@ include('header.php')
         </div>
       </div>
     </div>
-  </main>
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
-</body>
-
-</html>
+<?php
+include('../footer.php');
+?>
