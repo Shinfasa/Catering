2d0071b123162156ddaa805d7d0d45eef8053133
reihanjanuwisesafatihattariansyah <reihan21012003@gmail.com>
2023-01-01 19:45:26 +0700

    Update faq.php

--- faq.php
+++ faq.php
@@ -18,24 +18,24 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        Accordion Item #1
+        1. Bagaimana cara membuat akun?
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>Anda dapat membuat akun dengan cara berikut:</strong> Masuk pada halaman Login > Pilih ikon Sign Up pada halaman Login > Kemudian masukkan Username anda > Masukkan E-mail yang Anda miliki > Masukkan Password > Setelah itu klik pada tombol Sign Up > Kemudian Anda akan diarahkan pada halaman Login dan Anda bisa Login dengan akun yang telah Anda buat.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+        2. Bagaimana cara untuk melakukan pemesanan dan pembayaran?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>Berikut cara untuk melakukan pemesanan pada WM Hana Asri.</strong> Pilih Menu yang akan di pesan pada tombol Kategori/Menu/Pada Rekomendasi Menu > Setelah itu Anda bisa melihat Detail Menu yang ada pada Menu > Kemudian Anda bisa memasukkan jumlah pemesanan yang diinginkan > Pilih tombol Add to Cart untuk memasukkan pemesanan pada Keranjang Anda > Masuk pada halaman Keranjang > Anda bisa melihat detail pesanan Anda pada keranjang > Kemudian klik tombol Check Out untuk melakukan Pemesanan > Pada halaman Check Out Anda 
       </div>
     </div>
   </div>
