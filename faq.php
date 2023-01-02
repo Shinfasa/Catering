a5bdb0223889fc3d7751b0fd8dfc0adbca1a0e72
reihanjanuwisesafatihattariansyah <reihan21012003@gmail.com>
2023-01-02 12:17:08 +0700

    Update faq.php

--- faq.php
+++ faq.php
@@ -23,7 +23,16 @@
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Anda dapat membuat akun dengan cara berikut:</strong> <br>Masuk pada halaman Login > Pilih ikon Sign Up pada halaman Login > Kemudian masukkan Username anda > Masukkan Email yang Anda miliki > Masukkan Password > Setelah itu klik pada tombol Sign Up > Kemudian Anda akan diarahkan pada halaman Login dan Anda bisa Login dengan akun yang telah Anda buat.
+        <ul data-role="list">
+          <strong>Anda dapat membuat akun dengan cara berikut:</strong> 
+          <li>Masuk pada halaman Login</li> 
+          <li>Pilih ikon Sign Up pada halaman Login</li>
+          <li>Kemudian masukkan Username anda</li>
+          <li>Masukkan Email yang Anda miliki</li>
+          <li>Masukkan Password</li>
+          <li>Setelah itu klik pada tombol Sign Up</li>
+          <li>Kemudian Anda akan diarahkan pada halaman Login dan Anda bisa Login dengan akun yang telah Anda buat.</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -35,7 +44,20 @@
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Berikut cara untuk melakukan pemesanan pada WM Hana Asri.</strong> <br>Pilih Menu yang akan di pesan pada tombol Kategori/Menu/Pada Rekomendasi Menu > Setelah itu Anda bisa melihat Detail Menu yang ada pada Menu > Kemudian Anda bisa memasukkan jumlah pemesanan yang diinginkan > Pilih tombol Add to Cart untuk memasukkan pemesanan pada Keranjang Anda > Masuk pada halaman Keranjang > Anda bisa melihat detail pesanan Anda pada keranjang > Kemudian klik tombol Check Out untuk melakukan Pemesanan > Pada halaman Check Out Anda harus memasukkan detail pembayaran Anda > Setelah itu klik tombol Check Out > Kemudian Anda diharus kan mengupload Bukti Transfer Pembayaran > Sekarang Anda telah melakukan pemesanan dan pembayaran, Anda bisa melihat detail pesanan pada Pesanan Saya.
+        <ul data-role="list">
+          <strong>Berikut cara untuk melakukan pemesanan pada WM Hana Asri.</strong>
+          <li>Pilih Menu yang akan di pesan pada tombol Kategori/Menu/Pada Rekomendasi Menu</li>
+          <li>Setelah itu Anda bisa melihat Detail Menu yang ada pada Menu</li>
+          <li>Kemudian Anda bisa memasukkan jumlah pemesanan yang diinginkan</li>
+          <li>Pilih tombol Add to Cart untuk memasukkan pemesanan pada Keranjang Anda</li>
+          <li>Masuk pada halaman Keranjang</li>
+          <li>Anda bisa melihat detail pesanan Anda pada keranjang</li>
+          <li>Kemudian klik tombol Check Out untuk melakukan Pemesanan</li>
+          <li>Pada halaman Check Out Anda harus memasukkan detail pembayaran Anda</li>
+          <li>Setelah itu klik tombol Check Out</li>
+          <li>Kemudian Anda diharus kan mengupload Bukti Transfer Pembayaran</li>
+          <li>Sekarang Anda telah melakukan pemesanan dan pembayaran, Anda bisa melihat detail pesanan pada Pesanan Saya.</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -47,7 +69,12 @@
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Anda dapat melihat Riwayat Pesanan Anda dengan cara berikut:</strong> <br>Pada halaman Beranda terdapat tombol Pesanan Saya > Kemudian klik dan Anda akan diarahkan pada halaman Pesanan Saya > Dalam halaman Pesanan Saya Anda dapat melihat detail Pesanan Anda dan Riwayat Pesanan Anda.
+        <ul data-role="list">
+          <strong>Anda dapat melihat Riwayat Pesanan Anda dengan cara berikut:</strong>
+          <li>Pada halaman Beranda terdapat tombol Pesanan Saya</li>
+          <li>Kemudian klik dan Anda akan diarahkan pada halaman Pesanan Saya</li>
+          <li>Dalam halaman Pesanan Saya Anda dapat melihat detail Pesanan Anda dan Riwayat Pesanan Anda.</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -59,7 +86,13 @@
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Berikut Prosedur Pembayaran :</strong> <br>Setelah Anda memasukkan detail Pembayaran Anda pada halaman Check Out > Terdapat pilihan bank untuk melakukan metode pembayaran > Setelah Anda memilih metode pembayaran, Anda diharuskan melakukan transaksi pada akun rekening yang telah di cantumkan > Kemudian setelah anda melakukan transaksi pembayaran Anda diharuskan mengupload file bukti transaksi pembayaran.
+        <ul data-role="list">
+          <strong>Berikut Prosedur Pembayaran :</strong>
+          <li>Setelah Anda memasukkan detail Pembayaran Anda pada halaman Check Out</li>
+          <li>Terdapat pilihan bank untuk melakukan metode pembayaran</li>
+          <li>Setelah Anda memilih metode pembayaran, Anda diharuskan melakukan transaksi pada akun rekening yang telah di cantumkan</li>
+          <li>Kemudian setelah anda melakukan transaksi pembayaran Anda diharuskan mengupload file bukti transaksi pembayaran.</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -71,7 +104,10 @@
     </h2>
     <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Berikut jika Anda telat melakukan pembayaran lebih dari kurun waktu 24 jam.</strong> <br>Jika anda telat melakukan pembayaran lebih dari 24 jam maka Pesanan Anda yang ingin di Check Out akan otomatis di cancel atau dibatalkan, Maka dari itu lakukan pembayaran sebelum 24 jam jika pesanan Anda tidak ingin dibatalkan.
+        <ul data-role="list">
+          <strong>Berikut jika Anda telat melakukan pembayaran lebih dari kurun waktu 24 jam.</strong>
+          <li>Jika anda telat melakukan pembayaran lebih dari 24 jam maka Pesanan Anda yang ingin di Check Out akan otomatis di cancel atau dibatalkan, Maka dari itu lakukan pembayaran sebelum 24 jam jika pesanan Anda tidak ingin dibatalkan.</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -83,7 +119,15 @@
     </h2>
     <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>Customer Service.</strong> <br>Apabila Anda memiliki pertanyaan lebih lanjut atau mengalami kendala saat menggunakan WM Hana Asri, silahkan hubungi No.Telp dan Email yang tercantum dibawah ini untuk informasi lebih lanjut: <br><strong>No. Telepon</strong><br>+62 858 1531 3767 <br>+62 822 2883 5524 <br><strong>Email</strong><br>wm.hanaasri@gmail.com
+        <ul data-role="list">
+        <strong>Customer Service.</strong>
+        <br>Apabila Anda memiliki pertanyaan lebih lanjut atau mengalami kendala saat menggunakan WM Hana Asri, silahkan hubungi No.Telp dan Email yang tercantum dibawah ini untuk informasi lebih lanjut:
+        <br><strong>No. Telepon</strong>
+        <li>+62 858 1531 3767</li>
+        <li>+62 822 2883 5524</li>
+        <br><strong>Email</strong>
+        <li>wm.hanaasri@gmail.com</li>
+        </ul>
       </div>
     </div>
   </div>
