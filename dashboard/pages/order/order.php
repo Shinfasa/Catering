a508dfcd88c2a0ada448c69771219401abb087a7
Vernhurn <yasinalfaruq02@gmail.com>
2023-01-03 13:22:32 +0700

    Update order.php

--- dashboard/pages/order/order.php
+++ dashboard/pages/order/order.php
@@ -67,7 +67,7 @@ if(isset($_GET['id_order'])){
               </thead>
               <tbody>
                 <?php 
-                $batas = 9;
+                $batas = 7;
                 $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
                 $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
 
