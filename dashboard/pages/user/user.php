e8218a5c5987d77ad2b8cc615256b1949105f2bf
Vernhurn <yasinalfaruq02@gmail.com>
2023-01-03 14:05:53 +0700

    Update user.php

--- dashboard/pages/user/user.php
+++ dashboard/pages/user/user.php
@@ -53,6 +53,20 @@ if(isset($_GET['id_user'])){
     echo "<script>location='user.php'</script>";
   }
 }
+
+$batas = 5;
+                $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+                $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
+
+                $previous = $halaman - 1;
+                $next = $halaman + 1;
+
+                $data = mysqli_query($koneksi,"SELECT * FROM user JOIN akses ON user.id_akses = akses.id_akses;");
+                $jumlah_data = mysqli_num_rows($data);
+                $total_halaman = ceil($jumlah_data / $batas);
+
+                $data_pegawai = mysqli_query($koneksi,"SELECT * FROM user JOIN akses ON user.id_akses = akses.id_akses LIMIT $halaman_awal, $batas");
+                $nomor = $halaman_awal+1;
 ?>
 
 <div class="container-fluid py-3">
@@ -84,19 +98,7 @@ if(isset($_GET['id_user'])){
               </thead>
               <tbody>
                 <?php 
-                $batas = 5;
-                $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
-                $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;  
-
-                $previous = $halaman - 1;
-                $next = $halaman + 1;
-
-                $data = mysqli_query($koneksi,"SELECT * FROM user JOIN akses ON user.id_akses = akses.id_akses;");
-                $jumlah_data = mysqli_num_rows($data);
-                $total_halaman = ceil($jumlah_data / $batas);
-
-                $data_pegawai = mysqli_query($koneksi,"SELECT * FROM user JOIN akses ON user.id_akses = akses.id_akses LIMIT $halaman_awal, $batas");
-                $nomor = $halaman_awal+1;
+                
                 while($d = mysqli_fetch_array($data_pegawai)){
                   ?>
                   <tr>
